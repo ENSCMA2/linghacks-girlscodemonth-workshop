ecc8e51efd65a4d12ec352801b025b689a818eb1
ENSCMA2 <karina.halevy@gmail.com>
2018-09-05 22:38:05 -0700

    spam filter problem with good and bad solutions

--- runner.py
+++ runner.py
@@ -6,37 +6,37 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from sklearn.preprocessing import LabelBinarizer
+
 def load_data(sheet):
-    csvfile = open(sheet, 'r+')
+    csvfile = open(sheet, encoding = "ISO-8859-1",mode='r+')
     lines = csv.reader(csvfile)
     data = list(lines)
     data = data[1:]
-    x = np.array([point[2] for point in data])
+    x = np.array([point[1] for point in data])
+    print(x[0])
     y = np.array([point[0] for point in data])
-    lb = LabelBinarizer()
+    print(y[0])
     X_TRAIN, X_TEST , Y_TRAIN, Y_TEST = train_test_split(x,y,test_size=.2)
-    return X_TRAIN, X_TEST, lb.fit_transform(Y_TRAIN), lb.transform(Y_TEST)
+    return X_TRAIN, X_TEST, Y_TRAIN, Y_TEST
 
-train_data, test_data, train_labels, test_labels = load_data('ExtractedTweets.csv')
+train_data, test_data, train_labels, test_labels = load_data('spam.csv')
 vectorizer = TfidfVectorizer()
 vectorised_train_data = vectorizer.fit_transform(train_data)
 
 classifier = LinearSVC()
 classifier.fit(vectorised_train_data, train_labels)
 
-vectorised_test_data = vectorizer.transform(test_data)
+print("Training complete!")
 
-predictions = classifier.predict(vectorised_test_data)
+user_input = input("Send me a message!\n")
+user_data = np.array([user_input])
+vectorised_user_data = vectorizer.transform(user_data)
 
-accuracy = accuracy_score(test_labels, predictions)
-precision = precision_score(test_labels, predictions)
-recall = recall_score(test_labels, predictions)
-f1 = f1_score(test_labels, predictions)
-
-
-print(str(accuracy))
-print(str(precision))
-print(str(recall))
-print(str(f1))
+while(user_input != "quit"):
+	prediction = classifier.predict(vectorised_user_data)
+	print(prediction)
+	user_input = input("Send me a message!\n")
+	user_data = np.array([user_input])
+	vectorised_user_data = vectorizer.transform(user_data)
+print("Bye!")
 
