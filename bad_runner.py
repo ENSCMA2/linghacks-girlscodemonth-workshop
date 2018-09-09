de55f45d8acbc6004c0483c1100da7196e16d5a0
Karina Halevy <karina.halevy@gmail.com>
2018-09-08 17:31:26 -0700

    removed extra imports

--- bad_runner.py
+++ bad_runner.py
@@ -2,9 +2,7 @@ import csv
 import numpy as np
 from sklearn.svm import LinearSVC, SVC, NuSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 def load_data(sheet):
